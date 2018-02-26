c7343e79926e39e420e18d53104e2debe3a2c76b
jjesusmonroy <ppchuyjjms@gmail.com>
2018-02-26 15:42:01 -0700

    1st commit, created database

---
+++ app/src/main/java/com/example/jjesusmonroy/appwithsqlite/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jjesusmonroy.appwithsqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BDHandler db= new BDHandler(this);
+        db.consultar("Select * from alumnos");
+    }
+}
